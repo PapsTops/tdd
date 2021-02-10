2223ebffa61d68c564633c7387b62e3389de4636
Enriquez, Christopher B AUTOSOL SYSS MNL <christopher.enriquez@emerson.com>
2021-02-10 16:49:21 +0800

    Add calculator tests

---
+++ src/tdd/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd.tests
+{
+    public class Calculator
+    {
+        public string DetermineOutputBaseOnMultiple(int input)
+        {
+            if (input % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (input % 5 == 0)
+            {
+                return "buzz";
+            }
+
+            return "notfizz";
+        }
+
+        public string IsMultipleOfFive(int input)
+        {
+            if (input % 5 == 0)
+            {
+                return "fizz";
+            }
+
+            return "notfizz";
+        }
+    }
+}
--- tests/tdd.tests/UnitTest1.cs
+++ tests/tdd.tests/UnitTest1.cs
@@ -6,9 +6,64 @@ namespace tdd.tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ShouldNotOutputFizzIfInputIsNotThree()
         {
+            //Arrange
+            //init
+            var calculator = new Calculator();
 
+            //Act
+            var output = calculator.DetermineOutputBaseOnMultiple(5);
+
+            //Assert
+            Assert.NotEqual("fizz", output);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void ShouldOutputFizzIfInputIsMultipleOfThree(int input)
+        {
+            //Arrange
+            //init
+            var calculator = new Calculator();
+
+            //Act
+            var output = calculator.DetermineOutputBaseOnMultiple(input);
+
+            //Assert
+            Assert.Equal("fizz", output);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void ShouldOutputBuzzIfInputIsMultipleOfFive(int input)
+        {
+            //Arrange
+            //init
+            var calculator = new Calculator();
+
+            //Act
+            var output = calculator.DetermineOutputBaseOnMultiple(input);
+
+            //Assert
+            Assert.Equal("buzz", output);
+        }
+
+        [Fact]
+        public void ShouldNotOutputBuzzIfInputIsOne()
+        {
+            //Arrange
+            //init
+            var calculator = new Calculator();
+
+            //Act
+            var output = calculator.DetermineOutputBaseOnMultiple(1);
+
+            //Assert
+            Assert.NotEqual("buzz", output);
         }
     }
 }
